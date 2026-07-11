0fb9187df697704e79d8670ea155df4e2fa8ad7b
trinhphuong102 <tantrinhvl@gmail.com>
2026-07-11 16:29:33 +0700

    Create half_adder.v

---
+++ half_adder/rtl/half_adder.v
@@ -0,0 +1,10 @@
+module half_adder ( a , b , sum , cout);
+   input wire a , b ;
+   output reg sum , cout;
+ always @(a , b ) 
+   begin
+      sum = a ^b;
+      cout = a & b;
+   end
+  
+endmodule
